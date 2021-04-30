6696ca10f26fb908c3d97be10ed758840fd89377
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-04-29 21:25:31 -0600

    :update

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -5,8 +5,8 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {username: 'Britney', email: 'Britney@email.com', password: 'BP-Pass', country: 'USA'},
-        {username: 'Paulo', email: 'Paulo@gmail.com', password: 'PA-Pass', country: 'USA'},
+        {username: 'jon', email: 'jdeltoro88@gmail.com', password: 'BP-Pass', country: 'USA'},
+        
         // {username: '', email_address: 'email address', password: 'password', country: 'country', image_url: 'image_url'},
         // {username: 'username', email_address: 'email address', password: 'password', country: 'country', image_url: 'image_url'},
         // {username: 'username', email_address: 'email address', password: 'password', country: 'country', image_url: 'image_url'},
